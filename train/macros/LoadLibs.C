9317f3c3873b613155b74e3ef5b58b886d9e1169
mfasel <markus.fasel@cern.ch>
2016-02-05 15:49:08 +0100

    Load pythia library as well

--- train/macros/LoadLibs.C
+++ train/macros/LoadLibs.C
@@ -3,7 +3,7 @@ void LoadLibs(){
 	 * Loading fastjet libraries - Required for jet libraries
 	 */
 
-	TString libs[] = {"CGAL","fastjet","siscone","siscone_spherical","fastjetplugins","fastjettools","fastjetcontribfragile"};
+	TString libs[] = {"pythia6", "CGAL","fastjet","siscone","siscone_spherical","fastjetplugins","fastjettools","fastjetcontribfragile"};
 	for(TString *libiter = libs; libiter < libs + sizeof(libs)/sizeof(TString); libiter++){
 		gSystem->Load(Form("lib%s", libiter->Data()));
 	}
