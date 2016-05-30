1a4ea7b1daf6dc36166dee9453ff2004296c4f97
mfasel <markus.fasel@cern.ch>
2016-05-30 22:32:51 +0200

    Fix name of the AddTask

--- basics/ConfigTriggerMakerPP2016.C
+++ basics/ConfigTriggerMakerPP2016.C
@@ -1,6 +1,6 @@
 void ConfigTriggerMakerPP2016(){
 	gROOT->LoadMacro("$ALICE_PHYSICS/PWG/EMCAL/macros/AddTaskEmcalTriggerMakerNew.C");
-	AliEmcalTriggerMakerTask *triggermaker = AddTaskTriggerEmcalMakerNew("EmcalTriggers", "", "", kTRUE);
+	AliEmcalTriggerMakerTask *triggermaker = AddTaskEmcalTriggerMakerNew("EmcalTriggers", "", "", kTRUE);
 	triggermaker->SetUseL0Amplitudes(kFALSE);
 	triggermaker->SelectCollisionCandidates(AliVEvent::kAnyINT || AliVEvent::kEGA || AliVEvent::kEJE);
 	triggermaker->GetTriggerMaker()->ConfigureForPP2016();
